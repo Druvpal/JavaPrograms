4d7c7f4c8f850bb3de7b50d27d36307883c0e3ec
Druvpal <manish25jan10feb@gmail.com>
2023-09-29 19:33:12 +0530

    Super Key using Variable , Method and Constructuctor

--- superKey1.java
+++ superKey1.java
@@ -1,4 +1,4 @@
-class A{
+/*class A{
     int a=10;
 }
 
@@ -14,5 +14,47 @@ public class superKey1 {
     public static void main(String[] args) {
         B ref = new B();
         ref.show();
+    }
+}
+
+class A{
+    void show(){
+        System.out.println("Hello Manish");
+    }
+}
+
+class B extends A{
+    //int a =20;
+    void show(){
+        super.show();
+        System.out.println("Hello Kallu's");
+    }
+}
+
+class superKey1{
+    public static void main(String[] args) {
+        B ref = new B();
+        ref.show();
+    }
+}
+*/
+class A{
+    int a;
+    A(int x){
+        a=20;
+        System.out.println(a);
+    }
+}
+class B extends A{
+    int a =10;
+    B(){
+        super(20);
+        System.out.println(a);
+    }
+}
+
+class superKey1{
+    public static void main(String[] args) {
+        B ref = new B();
     }
 }
