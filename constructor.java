94addd1c8deec3a4a282ee0dd258fc3096e1dedb
Druvpal <manish25jan10feb@gmail.com>
2023-09-29 13:02:11 +0530

    Add constructor default and without default

---
+++ constructor.java
@@ -0,0 +1,19 @@
+public class constructor {
+    int a; String name;
+    //using default Constructor
+    // constructor(){
+    //     a = 10; name = "Manish";
+    // }
+    void show(){
+        System.out.println(a+ " " + name);
+    }
+
+}
+
+class constructor1 {
+    public static void main(String[] args) {
+        constructor x = new constructor();
+        //System.out.println(x.a + " "+x.name);
+        x.show();
+    }
+}
