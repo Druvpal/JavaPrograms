f7dea0f460830259c48e42bfee88e76daf961a8b
Manish Kumar Pal <145991162+Druvpal@users.noreply.github.com>
2023-11-22 22:25:24 +0530

    Print the 2D Array with the value start from the 11.

--- array2d.java
+++ array2d.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
 public class array2d {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of Array :");
-        int size = sc.nextInt();
-        int arr[][] = new int[size][size];
-
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                arr[i][j]=sc.nextInt();
+        System.out.println("Enter the row of the matrix");
+        int row = sc.nextInt();
+        System.out.println("Enter the column of the matrix :");
+        int column = sc.nextInt();
+        int arr[][] = new int[row][column];
+        int next =11;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                arr[i][j] = next;
+                next++;
             }
         }
-        System.out.println();
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                System.out.print(arr[i][j]+ " ");
+        System.out.println("Output of the matrix :");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
-        sc.close();
         
     }
 }
