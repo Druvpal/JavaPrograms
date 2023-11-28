d34c5d17197a6db5157c3999081ab9d9670a145a
Manish Kumar Pal <145991162+Druvpal@users.noreply.github.com>
2023-11-28 21:58:27 +0530

    Anagrams for the String.

--- check.java
+++ check.java
@@ -1,7 +1,62 @@
+import java.io.*;
+import java.util.*;
+
 public class check {
+
     public static void main(String[] args) {
-        String a = "Scaler";
-        String result = a.substring(2,4);
-        System.out.println(result);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+
+
+        if(a.length() != b.length()){
+            System.out.println("Not Anagrams");
+            System.exit(0);
+        }
+        else{
+            char arr[] = a.toCharArray();
+            char brr[] = b.toCharArray();
+
+            for(int i=0;i<a.length();i++){
+                if((int)arr[i] < 97){
+                    int temp = (int)arr[i] + 32;
+                    arr[i] = (char)temp;
+                }
+                else if ((int)brr[i] < 97){
+                    int temp = (int)brr[i] + 32;
+                    brr[i] = (char)temp;
+                }
+                else {
+                    continue;
+                }
+            }
+
+            Arrays.sort(arr);
+            Arrays.sort(brr);
+//            System.out.println();
+
+
+
+            boolean flag = false;
+            for(int i=0;i<a.length();i++){
+                if(arr[i] == brr[i] ){
+                    flag = true;
+                    System.out.println(arr[i] + " " + brr[i]);
+                }
+                else{
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag == true){
+                System.out.println("Anagrams");
+            }
+            else{
+                System.out.println("Not Anagrams");
+            }
+        }
     }
 }
