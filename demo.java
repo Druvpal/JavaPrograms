50be6d476e1b0ef9b2b61159cf818d42949f2f8a
Manish Kumar Pal <145991162+Druvpal@users.noreply.github.com>
2023-12-03 11:25:54 +0530

    Comperition Between Same parameter Integer

---
+++ demo.java
@@ -0,0 +1,22 @@
+public class demo {
+    public static void main(String[] args) {
+        Integer sum1 = 100;
+        Integer sum2 = 100;
+        Integer sum3 = 500;
+        Integer sum4 = 500;
+
+        if(sum1==sum2){
+            System.out.println("Sum1 == Sum2");
+        }
+        else{
+            System.out.println("Sum1 != Sum2");
+        }
+
+        if(sum3 == sum4){
+            System.out.println("Sum3 == Sum4");
+        }
+        else{
+            System.out.println("Sum3 != Sum4");
+        }
+    }
+}
